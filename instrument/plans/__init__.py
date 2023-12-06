1b30a1993be34c83bb203754eab87bb079257730
Jörg Strempfer <58751783+strempfer@users.noreply.github.com>
2023-12-06 10:23:31 -0600

    put scans back

--- instrument/plans/__init__.py
+++ instrument/plans/__init__.py
@@ -3,16 +3,16 @@
 local, custom Bluesky plans (scans) and other functions
 """
 
-# from .local_scans import (
-#     lup,
-#     ascan,
-#     mv,
-#     mvr,
-#     grid_scan,
-#     rel_grid_scan,
-#     # 'qxscan',
-#     count,
-#     abs_set
-# )
+from .local_scans import (
+    lup,
+    ascan,
+    mv,
+    mvr,
+    grid_scan,
+    rel_grid_scan,
+    # 'qxscan',
+    count,
+    abs_set
+)
 
-# from .center_maximum import maxi, cen
+from .center_maximum import maxi, cen
