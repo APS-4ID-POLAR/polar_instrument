2aba088a9475cb8ed1d8b9aa42d966ca675021af
Jörg Strempfer <58751783+strempfer@users.noreply.github.com>
2023-12-06 10:08:56 -0600

    change init order

--- instrument/devices/__init__.py
+++ instrument/devices/__init__.py
@@ -2,10 +2,10 @@
 local, custom Device definitions
 """
 
+from .simulated_scaler import scaler
 from .simulated_detector import simdet
 from .simulated_fourc_vertical import fourc
 from .simulated_new_diffractometer import diffract
 from .nanopositioner import nanopositioner
 from .interferometers_4IDG import interferometer
 from .simulated_sixc import psic, sixcpsi
-from .simulated_scaler import scaler
