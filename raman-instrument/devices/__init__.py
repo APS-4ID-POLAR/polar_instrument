46cf1f96fd8f7c10727ef7e0aac81c26ae3c73ce
Gilberto Fabbris <gfabbris@anl.gov>
2024-04-25 19:23:39 -0500

    Add Ventus Laser

--- raman-instrument/devices/__init__.py
+++ raman-instrument/devices/__init__.py
@@ -3,3 +3,4 @@ local, custom Device definitions
 """
 
 from .sample_stage import sx, sy, sz
+from .ventus_laser import laser
--- raman-instrument/devices/ventus_laser.py
+++ raman-instrument/devices/ventus_laser.py
@@ -4,15 +4,37 @@ Ventus Laser
 
 __all__ = ["laser"]
 
-from ophyd import EpicsMotor
+from ophyd import EpicsSignal, Component, Device
 from ..framework import sd
 from ..session_logs import logger
 logger.info(__file__)
 
 
-sx = EpicsMotor('4tst:m1', labels=('motor'), name="sx")
-sy = EpicsMotor('4tst:m2', labels=('motor'), name="sy")
-sz = EpicsMotor('4tst:m3', labels=('motor'), name="sz")
+class VentusLaser(Device):
 
-for item in [sx, sy, sz]:
-    sd.baseline.append(item)
+    #TODO: Should power and current be PVPositioners?
+    power_setpoint = Component(EpicsSignal, "PowerOut", kind="normal")
+    power_readback = Component(EpicsSignal, "PowerIn", kind="hinted")
+
+    current_setpoint = Component(EpicsSignal, "CurrentOut", kind="normal")
+    current_readback = Component(EpicsSignal, "CurrentIn", kind="normal")
+
+    laser_on = Component(EpicsSignal, "On", kind="ommited")
+    laser_off = Component(EpicsSignal, "Off", kind="ommited")
+
+    laser_temperature = Component(EpicsSignal, "LaserTemp", kind="config")
+    psu_temperature = Component(EpicsSignal, "PSUTemp", kind="config")
+
+    status = Component(EpicsSignal, "Status", kind="config")
+
+    scan = Component(EpicsSignal, "Read.SCAN", kind="config")
+
+    control_mode = Component(EpicsSignal, "ControlMode", kind="config")
+
+    psu_time = Component(EpicsSignal, "PSUTime", kind="config")
+    laser_enabled_time = Component(EpicsSignal, "LETime", kind="config")
+    laser_operation_time = Component(EpicsSignal, "LOTime", kind="config")
+
+
+laser = VentusLaser("4tst:LQE1:", name="laser")
+sd.baseline.append(laser)
