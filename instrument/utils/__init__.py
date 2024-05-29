be5eabdd26e6af85ba32a1f1b724f501abd4a85e
Gilberto Fabbris <gfabbris@anl.gov>
2024-05-28 19:52:24 -0500

    add find_eiger_triggers

--- instrument/utils/__init__.py
+++ instrument/utils/__init__.py
@@ -4,5 +4,5 @@ any extra commands or utility functions here
 """
 
 from .counters_class import counters
-from .flyscan_utils import read_flyscan_stream
+from .flyscan_utils import read_flyscan_stream, find_eiger_triggers
 # from .local_magics import LocalMagics
