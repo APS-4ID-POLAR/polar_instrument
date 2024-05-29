57c64a42d774a448ddbac1674694a6bc74018335
Gilberto Fabbris <gfabbris@anl.gov>
2024-05-29 12:18:39 -0500

    Add docs and cleanup

--- instrument/plans/flyscan_demo.py
+++ instrument/plans/flyscan_demo.py
@@ -105,7 +105,7 @@ def flyscan_1d(
 
     See Also
     --------
-    :func:`bluesky.plan_patterns.outter_product`
+    :func:`bluesky.plan_patterns.inner_product`
     :func:`flyscan_cycler`
     """
     detectors = [eiger]
@@ -122,11 +122,42 @@ def flyscan_1d(
 def flyscan_cycler(
         detectors,
         cycler,
-        speeds: list = [10],
+        speeds,
         trigger_time: float = 0.02,
         collection_time: float = 0.01,
         md: dict = {}
     ):
+
+    """
+    Flyscan using a generic path.
+
+    Note that only the last motor will fly (by doing a two point step).
+
+    Parameters
+    ----------
+    detectors : list of ophyd detectors
+        Currently sort of hardwired for the Eiger, which must be the first item in the
+        list. But this will be removed in the future to match with the POLAR standard of
+        defaulting to our `counters` class.
+    cycler : Cycler
+        cycler.Cycler object mapping movable interfaces to positions.
+    speeds : list
+        Velocity of the motors, this is particularly useful for the flying motor. If
+        `None`, then the speed will not be changed. The speed will be passed to
+        `motor.velocity` through staging (see ../devices/nanopositioners.py).
+    trigger_time : float, default to 0.02 seconds
+        Time between detector triggers.
+    collection_time : float, default to 0.01 seconds
+        Time that detector spend collecting the image. It must be smaller or equal to
+        the trigger_time otherwise a ValueError is raised.
+    md : dictionary, optional
+        Metadata to be added to the run start.
+
+    See Also
+    --------
+    :func:`bluesky.plan_patterns.outter_product`
+    :func:`bluesky.plan_patterns.inner_product`
+    """
 
     if collection_time > trigger_time:
         raise ValueError(
@@ -134,15 +165,11 @@ def flyscan_cycler(
             f"between triggers ({trigger_time})."
         )
 
-
     # TODO: For now we assume the eiger is the first detector
-    eiger_paths = detectors[0].hdf1.make_write_read_paths()    
-    # This again assumes the eiger is the first (and only detector)
-    # Changes the stage_sigs to the external trigger mode
-    # Staging already clicks the acquire button.
-    # detectors[0].setup_external_trigger()
+    eiger_paths = detectors[0].hdf1.make_write_read_paths()
     # Make sure eiger will save image
     detectors[0].auto_save_on()
+    # Changes the stage_sigs to the external trigger mode
     detectors[0]._flysetup = True
 
     # Metadata
@@ -186,12 +213,6 @@ def flyscan_cycler(
     yield from mv(*args)
 
     # Change motor speed
-    # For the PI stages, seems like the velocity has to be [6, 150] microns/sec
-    # _motor_speed_stash = yield from rd(motor.velocity)
-    # yield from mv(motor.velocity, speed)
-
-    # Testing...
-    # for motor in motors:
     speeds = speeds[::-1]  # The cycler inverts the motor list.
     for motor, speed in zip(motors, speeds):
         if speed is not None:
@@ -227,189 +248,3 @@ def flyscan_cycler(
         return (yield from null()) # Is there something better to do here?
 
     yield from inner_fly()
-
-    # # Metadata
-    # # TODO: More mds!
-    # motors = list(cycler.keys)
-    # _md = dict(
-    #     detectors = [det.name for det in detectors],
-    #     motors = [motor.name for motor in motors], # presumably we can have more later
-    #     plan_name = "flyscan_cycler",
-    #     # This assumes the first detector is the eiger.
-    #     eiger_file_path = detectors[0].hdf1.make_write_read_paths()[1],
-    #     # TODO: a similar scan with a monitor (scaler...)
-    #     hints = dict(monitor=None, detectors=[], scan_type="flyscan")
-    # )
-    # for item in detectors:
-    #     _md['hints']['detectors'].extend(item.hints['fields'])
-    
-    # dimensions = [(motor.hints["fields"], "primary") for motor in motors]
-    # _md["hints"].setdefault("dimensions", dimensions)
-
-    # _md.update(md)
-
-    # # Setup detectors
-    # for det in detectors:
-    #     yield from mv(det.preset_monitor, collection_time)
-    
-    # # This again assumes the eiger is the first (and only detector)
-    # # Changes the stage_sigs to the external trigger mode
-    # # Staging already clicks the acquire button.
-    # # detectors[0].setup_external_trigger()
-    # # Make sure eiger will save image
-    # detectors[0].auto_save_on()
-
-    # # Stop softglue just in case
-    # yield from sgz.stop_plan()
-
-    # # Setup the frequency
-    # yield from sgz.setup_trigger_time_plan(trigger_time)
-
-    # # Move motor to start position using "normal speed"
-    # # yield from mv(motor, start)
-
-    # # Change motor speed
-    # # For the PI stages, seems like the velocity has to be [6, 150] microns/sec
-    # # TODO: Is there a better way to handle this? If the scan is aborted, then the speed
-    # # will stay wrong.
-    # # _motor_speed_stash = {}
-    # # for motor in motors:
-    # #     _motor_speed_stash[motor] = yield from rd(motor.velocity)
-    # #     yield from mv(motor.velocity, speed)
-
-    # # Testing...
-    # for motor, speed in zip(motors, speeds):
-    #     motor.stage_sigs["velocity"] = speed
-
-    # detectors[0]._flysetup = True
-
-    # # Positioner stream file path
-    # positioner_stream.file_path = ""
-    # positioner_stream.file_name = ""
-
-    # @stage_decorator(list(detectors) + list(motors))
-    # @run_decorator(md=_md)
-    # def inner_fly():
-    #     yield from sgz.start_plan()
-        
-    #     pos_cache = defaultdict(lambda: None)
-    #     for step in list(cycler):
-    #         yield from move_per_step(step, pos_cache)
-    #     yield from sgz.stop_plan()
-    #     return (yield from null()) # Is there something better to do here?
-
-    # yield from inner_fly()
-
-    # # Put speed back
-    # # for motor, _speed in _motor_speed_stash.items():
-    # #     yield from mv(motor.velocity, _speed)
-
-    # # # Returns to manual trigger
-    # # detectors[0].setup_manual_trigger()
-
-# def flyscan_1d(
-#         detectors,
-#         motor,
-#         start,
-#         end,
-#         speed: float = 10,
-#         trigger_time: float = 0.02,
-#         collection_time: float = 0.01,
-#         md: dict = {},
-#     ):
-
-#     if collection_time > trigger_time:
-#         raise ValueError(
-#             f"The collection time ({collection_time}) cannot be larger than the time "
-#             f"between triggers ({trigger_time})."
-#         )
-    
-#     # TODO: For now we assume the eiger is the first detector
-#     eiger_paths = detectors[0].hdf1.make_write_read_paths()    
-#     # This again assumes the eiger is the first (and only detector)
-#     # Changes the stage_sigs to the external trigger mode
-#     # Staging already clicks the acquire button.
-#     # detectors[0].setup_external_trigger()
-#     # Make sure eiger will save image
-#     detectors[0].auto_save_on()
-#     detectors[0]._flysetup = True
-
-#     # Metadata
-#     # TODO: More mds?
-#     _md = dict(
-#         detectors = [det.name for det in detectors],
-#         motors = [motor.name], # presumably we can have more later
-#         plan_name = "flyscan_1d",
-#         # This assumes the first detector is the eiger.
-#         eiger_file_path = eiger_paths[1],
-#         # TODO: a similar scan with a monitor (scaler...)
-#         hints = dict(monitor=None, detectors=[], scan_type="flyscan")
-#     )
-#     for item in detectors:
-#         _md['hints']['detectors'].extend(item.hints['fields'])
-    
-#     dimensions = [(motor.hints["fields"], "primary")]
-#     _md["hints"].setdefault("dimensions", dimensions)
-
-#     _md.update(md)
-
-#     # Setup detectors
-#     for det in detectors:
-#         yield from mv(det.preset_monitor, collection_time)
-
-#     # Stop and reset softglue just in case
-#     yield from sgz.stop_eiger()
-#     yield from sgz.stop_softglue()
-#     yield from sgz.reset_plan()
-
-#     # Setup the frequency
-#     yield from sgz.setup_eiger_trigger_plan(trigger_time)
-#     # TODO: Should we change the speed of the interferometer?
-#     # yield from sgz.setup_interf_trigger_plan(trigger_time/1000)
-
-#     # Move motor to start position using "normal speed"
-#     yield from mv(motor, start)
-
-#     # Change motor speed
-#     # For the PI stages, seems like the velocity has to be [6, 150] microns/sec
-#     # _motor_speed_stash = yield from rd(motor.velocity)
-#     # yield from mv(motor.velocity, speed)
-
-#     # Testing...
-#     # for motor in motors:
-#     motor.stage_sigs["velocity"] = speed
-
-#     # Setup names in positioner_stream
-#     positioner_stream.file_path = eiger_paths[0]
-#     # TODO: Need a better way to handle this file name....
-#     positioner_stream.file_name = "positionstream_" + eiger_paths[1].split("/")[-1]
-
-#     @stage_decorator(list(detectors) + [motor])
-#     @run_decorator(md=_md)
-#     def inner_fly():
-#         yield from mv(positioner_stream, 1)
-#         yield from sgz.start_softglue()
-
-#         yield from sgz.start_eiger()
-#         yield from mv(motor, end)
-#         yield from sgz.stop_eiger()
-
-#         # This will wait for a full new set of packets.
-#         # TODO: It's an overkill, maybe Keenan's code can broadcast a signal?
-#         n = yield from rd(sgz.div_by_n_interf.n)
-#         _time_per_point = n/1e7
-#         _number_of_events_per_packet = 1e5/8
-#         yield from sleep(_time_per_point*_number_of_events_per_packet+ 0.1)
-
-#         yield from sgz.stop_softglue()
-#         yield from mv(positioner_stream, 0)
-
-#         return (yield from null()) # Is there something better to do here?
-
-#     yield from inner_fly()
-
-#     # Put speed back
-#     # yield from mv(motor.velocity, _motor_speed_stash)  
-
-#     # # Returns to manual trigger
-#     # detectors[0].setup_manual_trigger()
