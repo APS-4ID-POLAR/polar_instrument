929e6b8ad40814e5fa6ffefa63faaa9d57a37105
Gilberto Fabbris <gfabbris@anl.gov>
2024-06-03 18:56:01 -0500

    clean up

--- instrument/utils/__init__.py
+++ instrument/utils/__init__.py
@@ -5,5 +5,5 @@ any extra commands or utility functions here
 
 from .counters_class import counters
 from .flyscan_utils import read_flyscan_stream, find_eiger_triggers
-# from .setup_user import setup_user
+from .setup_user import setup_user
 # from .local_magics import LocalMagics
