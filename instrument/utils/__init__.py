0b3a6dba73e1310c02fb440bac55cfe519b3960c
Joerg Strempfer <joerg@strempfer.com>
2024-08-27 10:55:25 -0500

    add hkl_utils to init file

--- instrument/utils/__init__.py
+++ instrument/utils/__init__.py
@@ -6,5 +6,6 @@ any extra commands or utility functions here
 from .counters_class import counters
 from .utilities import *
 from .transfocator import *
+from .hkl_utils import *
 
 # from .local_magics import LocalMagics
