9bccc99e0459d4c81d8e8c6b335271f9ce611250
Gilberto Fabbris <gfabbris@anl.gov>
2024-08-27 18:17:38 -0500

    missed one CTR8 instance

--- instrument/utils/counters_class.py
+++ instrument/utils/counters_class.py
@@ -109,7 +109,7 @@ class CountersClass:
 
     @default_scaler.setter
     def default_scaler(self, value):
-        available = {0: scaler, 1: scaler_4tst, 2: scaler_ctr8}
+        available = {0: scaler, 1: scaler_4tst}
         if value is not None:
             if value in [item for _, item in available.items()]:
                 self._default_scaler = value
