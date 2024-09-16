6c1636bf0b126e668ba400057f0b204ad2b14f83
Gilberto Fabbris <gfabbris@anl.gov>
2024-09-16 16:23:45 -0500

    debug init

--- src/instrument/__init__.py
+++ src/instrument/__init__.py
@@ -2,14 +2,14 @@
 
 """instrument: Bluesky Data Acquisition in console, notebook, & queueserver."""
 
-__package__ = "instrument"
+# __package__ = "instrument"
 
-try:
-    from setuptools_scm import get_version
+# try:
+#     from setuptools_scm import get_version
 
-    __version__ = get_version(root="..", relative_to=__file__)
-    del get_version
-except (LookupError, ModuleNotFoundError):
-    from importlib.metadata import version
+#     __version__ = get_version(root="..", relative_to=__file__)
+#     del get_version
+# except (LookupError, ModuleNotFoundError):
+#     from importlib.metadata import version
 
-    __version__ = version(__package__)
+#     __version__ = version(__package__)
