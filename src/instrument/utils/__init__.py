fea970b14acf416000f8d2865a69b1849e3c8851
Gilberto Fabbris <gfabbris@anl.gov>
2024-09-23 17:14:19 -0500

    debug

--- src/instrument/utils/__init__.py
+++ src/instrument/utils/__init__.py
@@ -10,6 +10,7 @@ from .debug_setup import *  # noqa
 from .mpl_setup import *  # noqa
 from .hkl_utils import *
 from .transfocator import *
+from .experiment_setup import *
 
 from .flyscan_utils import read_flyscan_stream, find_eiger_triggers
 # from .dm_utils import setup_user, dm_get_experiment_data_path, get_processing_job_status
