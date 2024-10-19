d6d0df794926e24f5d8c11422d33794dbc247529
Gilberto Fabbris <gfabbris@anl.gov>
2024-10-19 13:23:38 -0500

    debug

--- src/instrument/plans/flyscan_demo.py
+++ src/instrument/plans/flyscan_demo.py
@@ -516,6 +516,7 @@ def flyscan_cycler(
 
     # TODO: this won't be needed when vortex uploads to voyager.
     # Upload vortex file
+    logger.info(_dets_file_paths["vortex"].parent, f"{RE.md['sample']}/vortex")
     upload_info = dm_upload(
         dm_experiment.get(),
         _dets_file_paths["vortex"].parent,
