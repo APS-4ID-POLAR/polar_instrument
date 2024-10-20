c3a426ff75318d2e1bb494559c76806d6a674369
Gilberto Fabbris <gfabbris@anl.gov>
2024-10-20 16:03:41 -0500

    debug

--- src/instrument/utils/dm_utils.py
+++ src/instrument/utils/dm_utils.py
@@ -167,7 +167,7 @@ def get_experiments_names(since="2018-01-01", until="2100-01-01"):
 	return names
 
 def current_run_experiments_names():
-    _run = get_current_run()
+	_run = get_current_run()
 	return get_experiments_names(since=_run["startTime"], until=run["endTime"])
 
 def get_proposal_info(proposal_id: int, run: str = None):
