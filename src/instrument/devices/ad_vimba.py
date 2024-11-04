ff7fac75e714533c51bb1aaada0236b66ec03082
Gilberto Fabbris <gfabbris@anl.gov>
2024-11-04 17:18:40 -0600

    debug vimba cam

--- src/instrument/devices/ad_vimba.py
+++ src/instrument/devices/ad_vimba.py
@@ -8,15 +8,16 @@ from ophyd.areadetector import CamBase, DetectorBase, ADComponent
 
 
 class VimbaCam(CamBase):
-    file_number_sync = None
-    file_number_write = None
-    fw_clear = None
-    link_0 = None
-    link_1 = None
-    link_2 = None
-    link_3 = None
-    dcu_buff_free = None
-    offset = None
+    # file_number_sync = None
+    # file_number_write = None
+    # fw_clear = None
+    # link_0 = None
+    # link_1 = None
+    # link_2 = None
+    # link_3 = None
+    # dcu_buff_free = None
+    # offset = None
+    pool_max_buffers = None
 
 
 class VimbaDetector(DetectorBase):
