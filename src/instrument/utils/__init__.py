999fa2d31b94669dcf4986fad88e2bee633c824d
Gilberto Fabbris <gfabbris@anl.gov>
2024-11-08 14:40:14 -0600

    debug

--- src/instrument/utils/__init__.py
+++ src/instrument/utils/__init__.py
@@ -14,7 +14,7 @@ from .dm_utils import (
     dm_upload,
     dm_upload_info,
 )
-from .oregistry_setup import oregistry, get_devices
+from .oregistry_setup import get_devices
 
 # from .dm_utils import (
 #   setup_user, dm_get_experiment_data_path, get_processing_job_status
