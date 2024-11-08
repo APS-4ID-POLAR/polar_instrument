4911be437b0b3588a21669f7fc29e8cdb2e304e2
Gilberto Fabbris <gfabbris@anl.gov>
2024-11-08 15:06:28 -0600

    add DOC

--- src/instrument/utils/oregistry_setup.py
+++ src/instrument/utils/oregistry_setup.py
@@ -7,6 +7,23 @@ oregistry = Registry(auto_register=False)
 
 
 def get_devices(label):
+    """
+    Prints a table with the devices that have the label.
+
+    Parameters
+    ----------
+    label : str
+        Label to be searched. This will be something like "scaler".
+
+    Returns
+    -------
+    None
+
+    See also
+    --------
+    :func:`ophydregistry.Registry.findall`
+    """
+
     objs = oregistry.findall(label=label, allow_none=True)
     table = {"Ophyd name": [], "PV prefix": [], "Label": []}
     for obj in objs:
