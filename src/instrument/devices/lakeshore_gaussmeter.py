bb073571fc0ec1bdb955ed999fc5b1c425c7b0b2
Gilberto Fabbris <gfabbris@anl.gov>
2024-12-06 10:28:37 -0600

    another PV typo

--- src/instrument/devices/lakeshore_gaussmeter.py
+++ src/instrument/devices/lakeshore_gaussmeter.py
@@ -6,7 +6,7 @@ from ophyd import Device, Component, EpicsSignalRO, EpicsSignal, Signal
 
 
 class GaussmeterDevice(Device):
-    field = Component(EpicsSignalRO, "Fld.val", kind="hinted")
+    field = Component(EpicsSignalRO, "Fld.VAL", kind="hinted")
     field_unit = Component(EpicsSignalRO, "FldUnit.SVAL", kind="normal")
     field_unit_setpoint = Component(EpicsSignal, "Unit.VAL", kind="omitted")
     scan = Component(EpicsSignal, "ReadFld.SCAN", kind="config")
@@ -18,5 +18,5 @@ class GaussmeterDevice(Device):
 
 
 gaussmeter = GaussmeterDevice(
-    "4idhSoft:Lakeshore475:", name="gaussmeter", labels=("detector",)
+    "4idhSoft:LakeShore475:", name="gaussmeter", labels=("detector",)
 )
