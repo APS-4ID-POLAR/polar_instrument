e6725c706bdceae299b57f99308f408b117e538c
Gilberto Fabbris <gfabbris@anl.gov>
2024-12-05 18:29:02 -0600

    tweak pvs

--- src/instrument/devices/lakeshore_gaussmeter.py
+++ src/instrument/devices/lakeshore_gaussmeter.py
@@ -7,8 +7,8 @@ from ophyd import Device, Component, EpicsSignalRO, EpicsSignal, Signal
 
 class GaussmeterDevice(Device):
     field = Component(EpicsSignalRO, "Fld.val", kind="hinted")
-    field_unit = Component(EpicsSignalRO, "Unit.VAL", kind="normal")
-    field_unit_setpoint = Component(EpicsSignal, "FldUnit.SVAL", kind="omitted")
+    field_unit = Component(EpicsSignalRO, "FldUnit.SVAL", kind="normal")
+    field_unit_setpoint = Component(EpicsSignal, "Unit.VAL", kind="omitted")
     scan = Component(EpicsSignal, "ReadFld.SCAN", kind="config")
     acquire_dummy = Component(Signal, value=0, kind="omitted")
 
