7cb6798f64e6b567a5d7ac50a58bdafc686cb87d
Gilberto Fabbris <gfabbris@anl.gov>
2024-12-06 11:22:32 -0600

    add offset to undulator

--- src/instrument/devices/s4idundulator.py
+++ src/instrument/devices/s4idundulator.py
@@ -3,11 +3,12 @@ Undulator support
 """
 
 from apstools.devices import STI_Undulator, TrackingSignal
-from ophyd import Component, Device
+from ophyd import Component, Device, Signal
 
 
 class PolarUndulator(STI_Undulator):
     tracking = Component(TrackingSignal, value=False, kind='config')
+    offset = Component(Signal, value=0, kind='config')
     version_hpmu = None
 
 
