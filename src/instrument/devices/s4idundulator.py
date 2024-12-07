b6f51cea0db6294aef5b420882883c2fe866c374
Gilberto Fabbris <gfabbris@anl.gov>
2024-12-06 19:25:24 -0600

    add check for no motion

--- src/instrument/devices/s4idundulator.py
+++ src/instrument/devices/s4idundulator.py
@@ -3,12 +3,39 @@ Undulator support
 """
 
 from apstools.devices import STI_Undulator, TrackingSignal
+from apstools.devices.aps_undulator import UndulatorPositioner
 from ophyd import Component, Device, Signal
+from ophyd.status import Status, StatusBase
+from typing import Any, Callable
+from numpy import abs
+
+
+class PolarUndulatorPositioner(UndulatorPositioner):
+
+    def set(
+        self,
+        new_position: Any,
+        *,
+        timeout: float = None,
+        moved_cb: Callable = None,
+        wait: bool = False,
+    ) -> StatusBase:
+        # If position is within the deadband --> do nothing.
+        if abs(new_position - self.get()) <= self.parent.energy_deadband.get():
+            _status = Status()
+            _status.set_finished()
+        else:
+            _status = super().set(
+                new_position, timeout=timeout, moved_cb=moved_cb, wait=wait
+            )
+        return _status
 
 
 class PolarUndulator(STI_Undulator):
     tracking = Component(TrackingSignal, value=False, kind='config')
     offset = Component(Signal, value=0, kind='config')
+    energy_deadband = Component(Signal, value=0.001, kind='config')
+    energy = Component(PolarUndulatorPositioner, "Energy")
     version_hpmu = None
 
 
