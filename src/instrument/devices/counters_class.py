6f4a192dd9a674a036e3a2d1d94863124351d66b
Gilberto Fabbris <gfabbris@anl.gov>
2024-12-08 10:03:52 -0600

    debug

--- src/instrument/devices/counters_class.py
+++ src/instrument/devices/counters_class.py
@@ -285,7 +285,10 @@ class CountersClass:
             det = oregistry.find(name)
             print("\n", name, "\n", group)
             # det.select_plot(item) selects that channel to plot.
-            # getattr(det, "select_plot")(group["channels"].values)
+            func = getattr(det, "select_plot")
+            print("0")
+            print(group["channels"].values)
+            func(group["channels"].values)
             print("1")
             dets.append(det)
 
