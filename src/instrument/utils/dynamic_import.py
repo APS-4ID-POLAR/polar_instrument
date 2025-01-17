a2729d7aff4fdafcf80d7d36e64d3b394c6c8c8d
Gilberto Fabbris <gfabbris@anl.gov>
2025-01-16 19:37:40 -0600

    debug

--- src/instrument/utils/dynamic_import.py
+++ src/instrument/utils/dynamic_import.py
@@ -17,8 +17,10 @@ def device_import(module_name, obj_name, baseline, timeout=TIMEOUT):
         module_path = f"instrument.devices.{module_name}"
         module = import_module(module_path)
         obj = getattr(module, obj_name)
-        obj.wait_for_connection(timeout=timeout, all_signals=True)
-        print(module_name, obj_name)
+        try:
+            obj.wait_for_connection(timeout=timeout, all_signals=True)
+        except TypeError:
+            obj.wait_for_connection(timeout=timeout)
     except (
         KeyError, NameError, TimeoutError, ConnectionTimeoutError
     ) as exinfo:
@@ -31,30 +33,27 @@ def device_import(module_name, obj_name, baseline, timeout=TIMEOUT):
         logger.warning(f"Setting {obj_name} to 'None'.")
         obj = None
 
-    # if obj is not None and baseline:
-    #     sd.baseline.append(obj)
+    if obj is not None and baseline:
+        sd.baseline.append(obj)
 
-    # cam = getattr(obj, "cam", None)
-    # if cam is not None:
-    #     cam.stage_sigs["wait_for_plugins"] = "Yes"
-    #     for nm in obj.component_names:
-    #         item = getattr(obj, nm)
-    #         if "blocking_callbacks" in dir(item):  # is it a plugin?
-    #             item.stage_sigs["blocking_callbacks"] = "No"
+    cam = getattr(obj, "cam", None)
+    if cam is not None:
+        cam.stage_sigs["wait_for_plugins"] = "Yes"
+        for nm in obj.component_names:
+            item = getattr(obj, nm)
+            if "blocking_callbacks" in dir(item):  # is it a plugin?
+                item.stage_sigs["blocking_callbacks"] = "No"
 
-    print("here")
+    hdf1 = getattr(obj, "hdf1", None)
+    if hdf1 is not None:
+        if obj.connected:
+            if not AD_plugin_primed(hdf1):
+                AD_prime_plugin2(hdf1)
 
-    # hdf1 = getattr(obj, "hdf1", None)
-    # if hdf1 is not None:
-    #     if obj.connected:
-    #         if not AD_plugin_primed(hdf1):
-    #             AD_prime_plugin2(hdf1)
+    defaults = getattr(obj, "default_settings", None)
+    if defaults is not None:
+        defaults()
 
-    # print("here")
-    # defaults = getattr(obj, "default_settings", None)
-    # if defaults is not None:
-    #     defaults()
-
-    # oregistry.register(obj)
+    oregistry.register(obj)
 
     return obj
