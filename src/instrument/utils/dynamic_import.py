9708692ae8d0b60dd4756f365c6b10b9d3ed3eac
Gilberto Fabbris <gfabbris@anl.gov>
2025-01-16 19:18:46 -0600

    tests

--- src/instrument/utils/dynamic_import.py
+++ src/instrument/utils/dynamic_import.py
@@ -17,6 +17,7 @@ def device_import(module_name, obj_name, baseline, timeout=TIMEOUT):
         module = import_module(module_path)
         obj = getattr(module, obj_name)
         obj.wait_for_connection(timeout=timeout)
+        print(module_name, obj_name)
     except (
         KeyError, NameError, TimeoutError, ConnectionTimeoutError
     ) as exinfo:
@@ -39,6 +40,7 @@ def device_import(module_name, obj_name, baseline, timeout=TIMEOUT):
             item = getattr(obj, nm)
             if "blocking_callbacks" in dir(item):  # is it a plugin?
                 item.stage_sigs["blocking_callbacks"] = "No"
+    print("here")
 
     hdf1 = getattr(obj, "hdf1", None)
     if hdf1 is not None:
@@ -46,6 +48,7 @@ def device_import(module_name, obj_name, baseline, timeout=TIMEOUT):
             if not AD_plugin_primed(hdf1):
                 AD_prime_plugin2(hdf1)
 
+    print("here")
     defaults = getattr(obj, "default_settings", None)
     if defaults is not None:
         defaults()
