84eab69db3b1d7142b2ca8f43fa29657a15e844b
Gilberto Fabbris <gfabbris@anl.gov>
2025-04-20 10:11:33 -0500

    add 4idg magnet

--- src/instrument/devices/electromagnet.py
+++ src/instrument/devices/electromagnet.py
@@ -6,7 +6,7 @@ Electromagnet
 __all__ = ['emag']
 
 from ophyd import Component, Device, EpicsMotor
-from .magnet_kepco import KepcoController
+from .magnet_kepco_4idb import KepcoController
 from ..utils._logging_setup import logger
 logger.info(__file__)
 
