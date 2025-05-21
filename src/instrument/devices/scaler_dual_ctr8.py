4ba786ec1576262f008da11b69cc33794f657bbe
gfabbris <gfabbris@anl.gov>
2025-05-20 20:39:57 -0500

    tests of new scaler

--- src/instrument/devices/scaler_dual_ctr8.py
+++ src/instrument/devices/scaler_dual_ctr8.py
@@ -42,7 +42,8 @@ class DualCTR8Scaler(Device):
         self.scaler1.channels.kind = Kind.omitted
         self.scaler2.channels.kind = Kind.omitted
 
-    def make_channels(self):
+    @classmethod
+    def make_channels(cls, attr_base, *, prefix1=None, prefix2=None):
         defn = OrderedDict()
 
         # First scaler
@@ -63,13 +64,56 @@ class DualCTR8Scaler(Device):
 
         return defn
 
-    channels = FormattedDynamicSubDevice(make_channels)
+    channels = FormattedDynamicSubDevice(lambda self: self.make_channels("bla"))
 
     scaler1 = FormattedComponent(ScalerCH, "{prefix1}")
     scaler2 = FormattedComponent(ScalerCH, "{prefix2}")
 
     preset_time = None
     preset_monitor = Component(PresetMonitorSignal, kind=Kind.config)
+
+    def match_names(self):
+        for s in self.channels.component_names:
+            getattr(self.channels, s).match_name()
+
+    def select_channels(self, chan_names=None):
+        """Select channels based on the EPICS name PV
+
+        Parameters
+        ----------
+        chan_names : Iterable[str] or None
+
+            The names (as reported by the ``channel.chname`` signal)
+            of the channels to select.
+            If ``None``, select **all** channels named in the EPICS scaler.
+        """
+        self.match_names()
+        name_map = {}
+        for s in self.channels.component_names:
+            scaler_channel = getattr(self.channels, s)
+            nm = scaler_channel.s.name  # as defined in self.match_names()
+            if len(nm) > 0:
+                name_map[nm] = s
+
+        if chan_names is None:
+            chan_names = name_map.keys()
+
+        read_attrs = []
+        for ch in chan_names:
+            try:
+                read_attrs.append(name_map[ch])
+            except KeyError:
+                raise RuntimeError(
+                    "The channel {} is not configured "
+                    "on the scaler.  The named channels are "
+                    "{}".format(ch, tuple(name_map))
+                )
+
+        self.channels.kind = Kind.normal
+        self.channels.read_attrs = list(read_attrs)
+        self.channels.configuration_attrs = list(read_attrs)
+        for ch in read_attrs[1:]:
+            getattr(self.channels, ch).s.kind = Kind.hinted
 
     @property
     def trigger_scaler(self):
